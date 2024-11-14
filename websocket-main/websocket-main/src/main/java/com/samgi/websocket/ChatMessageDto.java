1adec22323b452acc973c90e169e8bd9fc9daeba
kim6974 <wjdtjdnjs13@gmail.com>
2024-11-15 03:07:03 +0900

    Add files via upload

---
+++ websocket-main/websocket-main/src/main/java/com/samgi/websocket/ChatMessageDto.java
@@ -0,0 +1,18 @@
+package com.samgi.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessageDto {
+    private String roomId;
+    private String writer;
+    private String message;
+}
---
+++ websocket-main/websocket-main/src/main/java/com/samgi/websocket/ChatWebSocketHandler.java
@@ -0,0 +1,10 @@
+package com.samgi.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+}
